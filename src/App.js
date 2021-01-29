9e8ec1cbcaeef804d863695e6eaaee410681db4d
Tomo <tomo.vucic@gmail.com>
2021-01-29 16:19:03 +0100

    First commit. Initial setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+const App = function App() {
+  return (
+    <div className="App">
+      <div className="content">
+        <h1>React blog initial</h1>
+      </div>
+    </div>
+  );
+};
+
+export default App;
