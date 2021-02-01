8048e0a006fa7fb6f8671b3e9c3db0d55fbd68a3
Tomo <tomo.vucic@gmail.com>
2021-02-01 20:29:05 +0100

    data/db.json file created and fetching data from Home.js file in useEffect Hook

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,8 +1,7 @@
-const BlogList = ({ blogs,title }) => { // const BlogList = ( props ) => {
+const BlogList = ({ blogs, title }) => { // const BlogList = ( props ) => {
 
     // const blogs = props.blogs;
     // const title = props.title;
-    
     return ( 
         <div className="blog-list">
         <h2>{ title }</h2>
--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,32 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
 
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'codee', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'frontee', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'backee', id: 3 }
-    ]);
-    
+    const [blogs, setBlogs] = useState([]);
+    const [isPending, setIsPending] = useState(true);
+
+
+    useEffect(() => {
+        fetch('http://localhost:8000/blogs')
+            .then( res => {
+               if(res.ok){
+                  throw Error('could not fetch data from server');
+               }
+            })
+            .then(data => { 
+               console.log(data);
+               setBlogs(data);
+               setIsPending(false);
+            })
+            .catch((err) => {
+               console.log(err.message);
+            })
+    }, []);
+
     return ( 
         <div className="home">
+            { isPending && <div>Loading... </div> }
             <BlogList blogs={ blogs } title="All blogs" />
         </div>
      );
