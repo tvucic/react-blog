05d129035b35321cc153006415cd63e832222850
Tomo <tomo.vucic@gmail.com>
2021-02-02 09:45:57 +0100

    externalize useFetch hook and implement routing in app

---
+++ src/useFetch.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react';
+
+const useFetch = (url) => {
+
+    const [data, setData] = useState([]);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        fetch(url)
+            .then( res => {
+               if(!res.ok){
+                  throw Error('could not fetch data from server');
+               }
+               return res.json();
+            })
+            .then(data => { 
+               console.log(data);
+               setData(data);
+               setIsPending(false);
+               setError(null);
+            })
+            .catch((err) => {
+               setIsPending(false);
+               setError(err.message);
+            })
+    }, [url]);
+
+    return { data, isPending, error }
+}
+
+export default useFetch;
